f0cb998802d16f61130a6ed0fe7756d204abd2d4
seo-faper <dswhdcks@gmail.com>
2023-09-20 04:09:37 +0900

    파일 선택 테스트

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 from PyQt5.QtGui import QIcon
 
 form_class = uic.loadUiType("./main.ui")[0]
@@ -9,13 +9,26 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setMinimumSize(800, 600) 
+        self.setMinimumSize(800, 600)
+
         self.setWindowTitle('문서이메일탈탈털기툴')
-        self.setWindowIcon(QIcon('./res/icon.png'))   
+        self.setWindowIcon(QIcon('./res/icon.png'))
+
+        self.actionopen.triggered.connect(self.open_file_dialog)
+
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly  # 읽기 전용으로 파일 열기
+
+        file_dialog = QFileDialog()
+        file_name, _ = file_dialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt);;All Files (*)", options=options)
+
+        if file_name:
+            print("Selected file path:", file_name)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = WindowClass()
-    
+
     myWindow.show()
     app.exec_()
