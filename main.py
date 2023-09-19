990d5ce6a3335b75a4bb4dfeccd891625ac2d640
seo-faper <dswhdcks@gmail.com>
2023-09-19 22:50:20 +0900

    창 여러개 열기

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtGui import QIcon
 
 form_class = uic.loadUiType("./main.ui")[0]
@@ -9,8 +9,9 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setMinimumSize(800, 600) 
         self.setWindowTitle('문서이메일탈탈털기툴')
-        self.setWindowIcon(QIcon('icon.png'))
+        self.setWindowIcon(QIcon('icon.png'))   
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
