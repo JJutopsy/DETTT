c9d94a968d84fd5b20674764919097e76eadd7e0
seo-faper <dswhdcks@gmail.com>
2023-09-21 19:06:52 +0900

    file select / open 구현

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog, QDialog, QDialogButtonBox, QVBoxLayout
 from PyQt5.QtGui import QIcon
 
 form_class = uic.loadUiType("./main.ui")[0]
+newCase_class = uic.loadUiType("./newCase.ui")[0]
 
-class CaseInputDialog(QDialog):
+class CaseInputDialog(QDialog, newCase_class):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Create New Case")
 
-        layout = QVBoxLayout()
 
-        self.case_name_label = QLabel("Case Name:")
-        self.case_name_input = QLineEdit()
+        self.setupUi(self)
 
-        self.folder_button = QPushButton("Select Folder")
         self.folder_button.clicked.connect(self.select_folder)
+        self.folder_input.setReadOnly(True)
 
-        self.folder_input = QLineEdit()  # 폴더 경로를 표시할 QLineEdit 추가
-        self.folder_input.setReadOnly(True)  # 읽기 전용으로 설정
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
 
-        self.ok_button = QPushButton("OK")
-        self.ok_button.clicked.connect(self.accept)
-        self.ok_button.setEnabled(False)  # 처음에는 비활성화
+        self.button_box.button(QDialogButtonBox.Ok).setEnabled(False)
 
-        layout.addWidget(self.case_name_label)
-        layout.addWidget(self.case_name_input)
-        layout.addWidget(self.folder_button)
-        layout.addWidget(self.folder_input)
-        layout.addWidget(self.ok_button)
-
-        self.setLayout(layout)
-
-        # 텍스트 입력 필드가 변경될 때마다 체크
         self.case_name_input.textChanged.connect(self.check_inputs)
 
     def select_folder(self):
@@ -46,10 +34,9 @@ class CaseInputDialog(QDialog):
             self.check_inputs()
 
     def check_inputs(self):
-        # 두 입력 필드 모두 값이 있을 때 "OK" 버튼 활성화
         case_name = self.case_name_input.text()
         folder_path = self.folder_input.text()
-        self.ok_button.setEnabled(bool(case_name) and bool(folder_path))
+        self.button_box.button(QDialogButtonBox.Ok).setEnabled(bool(case_name) and bool(folder_path))
 
     def accept(self):
         super().accept()
@@ -64,9 +51,8 @@ class WindowClass(QMainWindow, form_class):
         self.setWindowTitle('문서이메일탈탈털기툴')
         self.setWindowIcon(QIcon('./res/icon.png'))
 
-        # "actionnew_2" 메뉴 아이템에 대한 클릭 이벤트 핸들러 연결
         self.actionnew_2.triggered.connect(self.show_case_input_dialog)
-
+        self.actionopen.triggered.connect(self.open_case_dialog)
     def show_case_input_dialog(self):
         case_input_dialog = CaseInputDialog()
         result = case_input_dialog.exec_()
@@ -77,6 +63,15 @@ class WindowClass(QMainWindow, form_class):
             print(f"Case Name: {case_name}")
             print(f"Selected Folder: {folder_path}")
 
+    def open_case_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly 
+
+        file_dialog = QFileDialog()
+        file_name, _ = file_dialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt);;All Files (*)", options=options)
+
+        if file_name:
+            print("Selected file path:", file_name)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
