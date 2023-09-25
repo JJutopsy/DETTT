638c2d91ebb8869cba312881ed89c76c1d17a4b8
Seo-Faper <dswhdcks@gmail.com>
2023-09-25 09:29:19 +0900

    TreeView 개발 시작

---
+++ tree.py
@@ -0,0 +1,71 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeView, QVBoxLayout, QWidget
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt
+class FileTreeView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('File TreeView Example')
+        self.setGeometry(100, 100, 800, 600)
+        
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+        
+        self.treeView = QTreeView()
+        layout.addWidget(self.treeView)
+        
+        # SQLite3 데이터베이스 연결
+        self.conn = sqlite3.connect('file_path.db')  
+        self.cursor = self.conn.cursor()
+        
+
+        self.cursor.execute('SELECT file_path FROM dirTest')
+        
+        self.model = QStandardItemModel()
+        self.populate_treeview()
+        
+        self.treeView.setModel(self.model)
+
+    def populate_treeview(self):
+        for row in self.cursor.fetchall():
+            file_path = row[0]
+           
+            self.add_path_to_treeview(file_path)
+
+    def add_path_to_treeview(self, path):
+        root = self.model.invisibleRootItem()
+        path_parts = path.split("\\")  # 파일 경로를 경로 부분으로 분할
+
+        # TreeView에 경로를 추가
+        current_item = root
+        for part in path_parts:
+            child_item = None
+            for row in range(current_item.rowCount()):
+                child = current_item.child(row)
+                if child.text() == part:
+                    
+                    child_item = child
+                    break
+            if not child_item:
+                child_item = QStandardItem(part)
+                current_item.appendRow(child_item)
+            current_item = child_item
+            # 아이템을 읽기 전용으로 설정
+            child_item.setFlags(child_item.flags() & ~Qt.ItemIsEditable)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = FileTreeView()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ treeDIR.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+
+# 검색할 디렉토리 경로 설정
+start_dir = 'C:\\Users\\dswhd\OneDrive\\문서\\디포 자료\\행복의류 증거데이터 v0811'  # 원하는 디렉토리 경로로 변경하세요.
+full_path = os.path.dirname(start_dir)
+# 찾을 확장자 리스트
+extensions_to_find = ['.pdf', '.eml', '.docx', '.xlsx']
+
+# SQLite3 데이터베이스 연결 및 테이블 생성
+conn = sqlite3.connect('file_path.db')  # 데이터베이스 파일명을 수정하세요.
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS dirTest (file_path TEXT, full_path TEXT)')
+
+# 디렉토리 탐색 함수 정의
+def search_files(directory):
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            # 파일 확장자 확인
+            _, file_extension = os.path.splitext(file_path)
+            file_path = file_path[len(full_path)+1:]
+            if file_extension.lower() in extensions_to_find:
+                # 데이터베이스에 파일 경로 저장
+                cursor.execute('INSERT INTO dirTest (file_path, full_path) VALUES (?, ?)', (file_path, full_path))
+                conn.commit()
+                print(f'파일: {file_path}')
+
+# 디렉토리 탐색 시작
+search_files(start_dir)
+
+# 데이터베이스 연결 닫기
+conn.close()
